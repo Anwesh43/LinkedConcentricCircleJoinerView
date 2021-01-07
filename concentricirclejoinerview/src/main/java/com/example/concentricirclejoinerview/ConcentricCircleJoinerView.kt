5a605e2129084c9655391380cc12ab4692bdecc0
Anwesh43 <anweshthecool0@gmail.com>
2021-01-08 02:14:49 +0530

    adding CCJNode class

--- concentricirclejoinerview/src/main/java/com/example/concentricirclejoinerview/ConcentricCircleJoinerView.kt
+++ concentricirclejoinerview/src/main/java/com/example/concentricirclejoinerview/ConcentricCircleJoinerView.kt
@@ -122,4 +122,45 @@ class ConcentricCircleJoinerView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CCJNode(var i : Int, val state : State = State()) {
+
+        private var next : CCJNode? = null
+        private var prev : CCJNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = CCJNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCCJNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CCJNode {
+            var curr : CCJNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }

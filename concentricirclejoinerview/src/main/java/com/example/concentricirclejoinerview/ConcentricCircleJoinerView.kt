a4fddb33c2da7b4414973f6eedde425632e9dd7e
Anwesh43 <anweshthecool0@gmail.com>
2021-01-08 02:01:47 +0530

    adding Canvas extension methods

--- concentricirclejoinerview/src/main/java/com/example/concentricirclejoinerview/ConcentricCircleJoinerView.kt
+++ concentricirclejoinerview/src/main/java/com/example/concentricirclejoinerview/ConcentricCircleJoinerView.kt
@@ -24,8 +24,38 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
+val lines : Int = 4
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawConcentricCircleJoiner(scale : Float, w : Float, h : Float, paint : Paint) {
+    val r1 : Float = Math.min(w, h) / r1Factor
+    val r2 : Float = Math.min(w, h) / r2Factor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2)
+    drawArc(RectF(-r2, -r2, r2, r2), 0f, 360f * sf1, false, paint)
+    drawArc(RectF(-r1, -r1, r1, r1), 0f, 360f * sf2, false, paint)
+    for (j in 0..(lines - 1)) {
+        save()
+        rotate((360f / lines) * j)
+        drawLine(r1, 0f, r1 + (r2 - r1) * sf3, 0f, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawCCJNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawConcentricCircleJoiner(scale, w, h, paint)
+}

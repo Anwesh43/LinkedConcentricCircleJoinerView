6e77751e7316c33548eef009c8b615b047a7ab9e
Anwesh43 <anweshthecool0@gmail.com>
2021-01-08 02:07:59 +0530

    adding Animator class

--- concentricirclejoinerview/src/main/java/com/example/concentricirclejoinerview/ConcentricCircleJoinerView.kt
+++ concentricirclejoinerview/src/main/java/com/example/concentricirclejoinerview/ConcentricCircleJoinerView.kt
@@ -94,4 +94,32 @@ class ConcentricCircleJoinerView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animated(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
